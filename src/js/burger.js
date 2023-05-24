d6cc9e264e4c92676a6be8d8375b7619e9609387
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-05-24 10:42:09 +0500

    поправил малость, бургер подключил

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,7 +1,11 @@
-document.querySelector(function () {
-    document.querySelector('.header__menu-btn').on('click', function () {
-        document.querySelector('.header__nav-main-list').classList.toggle('list--active');
-        document.querySelector('.header__menu-btn').classList.toggle('menu-btn--active');
-    });
-});
 
+
+
+const menuBtn = document.querySelector('.header__menu-btn');
+const menu = document.querySelector('.header__nav-main-list');
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('list--active');
+});
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('menu-btn--active');
+});
